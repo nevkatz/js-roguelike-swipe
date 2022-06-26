5b6e4bb13a42fa4a6363612ab74885079b2f1366
Nevin Katz <nevkatz@gmail.com>
2022-06-26 11:04:30 -0400

    added player object

---
+++ js/player.js
@@ -0,0 +1,56 @@
+/**
+ *  This is used for the scrolling version.
+ *  In the proto-swipe version, the player code is in script.js.
+ */ 
+
+/**
+ * Creates a new player. 
+ * @class
+ * 
+ * @property {number} level - starts at one and progresses
+ * @property {number} health - keep this above zero
+ * @property {string} weapon - ties to an object with a damage rating
+ * @property {object} coords - location on the grid
+ * @property {number} xp - experience points
+ * @property {relics} relics - relics collected
+ */
+class Player {
+   constructor(level, health, weapon, coords, xp, relics) {
+      this.level = level;
+      this.health = health;
+      this.weapon = weapon;
+      this.coords = coords;
+      this.relics = relics;
+      this.xp = xp;
+   }
+}
+Player.prototype.blockedY = function(diff) {
+
+        let topBounds = 0;
+        let bottomBounds = ROWS;
+        let atTop = diff.y < 0 && this.coords.y <= topBounds;
+        let atBot = diff.y > 0 && this.coords.y >= bottomBounds;
+
+        if (atTop) {
+            console.log('at top...');
+            this.coords.y = topBounds;
+        } else if (atBot) {
+            this.coords.y = bottomBounds;
+        }
+
+        return atTop || atBot;
+};
+Player.prototype.blockedX = function(diff) {
+
+        let rightBounds = COLS;
+        let leftBounds = 0;
+        let atRight = diff.x > 0 && this.coords.x + TILE_DIM / 2 >= game.canvas.width;
+        let atLeft = diff.x < 0 && this.coords.x - TILE_DIM / 2 <= 0;
+
+        if (atRight) {
+            player.coords.x = rightBounds;
+        } else if (atLeft) {
+            player.coords.x = leftBounds;
+        }
+        return atRight || atLeft;
+}
