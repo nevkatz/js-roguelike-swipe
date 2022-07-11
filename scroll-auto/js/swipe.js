683c14e5f2ddcaca505dc04adc6c6bd6ac4b13d5
Nevin Katz <nevkatz@gmail.com>
2022-07-10 20:20:40 -0400

    added checks for number of touches in swipeStart and swipeMove

--- scroll-auto/js/swipe.js
+++ scroll-auto/js/swipe.js
@@ -1,36 +1,39 @@
 /**
  * Swipe Logic
  */
-
 function swipeStart(e) {
     e.preventDefault();
-  
-     let obj = e.touches[0];
 
-     let {clientX,clientY} = obj;
+    if (e.touches.length == 1) {
+        let obj = e.touches[0];
 
-     game.touchCoords.x = clientX;
-     game.touchCoords.y = clientY;
+        let {clientX, clientY} = obj;
 
+        game.touchCoords.x = clientX;
+        game.touchCoords.y = clientY;
+
+    }
 }
+
 function swipeEnd(e) {
     e.preventDefault();
     player.velocity = {
-        x:0,
-        y:0
+        x: 0,
+        y: 0
     };
     window.clearInterval(game.timer);
     window.clearTimeout(game.timer);
 
     game.timer = null;
 }
+
 function setVelocityX(diff, clientX, clientY, newX, newY, min) {
     stopCoast();
     const coop_threshold = 10;
 
-    player.velocity.x = diff.x/Math.abs(diff.x);
+    player.velocity.x = diff.x / Math.abs(diff.x);
     game.touchCoords.x = clientX;
-            
+
     if (!player.edgeX(diff)) {
         newX = player.coords.x + player.velocity.x;
     }
@@ -41,38 +44,46 @@ function setVelocityX(diff, clientX, clientY, newX, newY, min) {
     }
     return newX;
 }
-function setVelocityY(diff,clientX,clientY,newX, newY, min) {
+
+function setVelocityY(diff, clientX, clientY, newX, newY, min) {
     stopCoast();
-    player.velocity.y = diff.y/Math.abs(diff.y);
+    player.velocity.y = diff.y / Math.abs(diff.y);
     game.touchCoords.y = clientY;
     if (!player.edgeY(diff)) {
         newY = player.coords.y + player.velocity.y;
     }
     // prevent buildup of small movements
     if (Math.abs(diff.x) < min && diff.x != 0) {
-           diff.x = 0;
-           game.touchCoords.x = clientX;
+        diff.x = 0;
+        game.touchCoords.x = clientX;
     }
     return newY;
 }
+
 function swipeMove(e) {
-        e.preventDefault();
+    e.preventDefault();
 
-        if (e.touches.length > 1) {
-            alert('number of touches: ' + e.touches.length);
-        }
-
+    if (e.touches.length == 1) {
         let obj = e.touches[0];
 
-        let {x:oldX, y:oldY} = player.coords;
+        let {
+            x: oldX,
+            y: oldY
+        } = player.coords;
 
-        let {x:newX, y:newY} = player.coords;
+        let {
+            x: newX,
+            y: newY
+        } = player.coords;
 
-        let {clientX, clientY} = obj;
+        let {
+            clientX,
+            clientY
+        } = obj;
 
         let diff = {
-           x:clientX - game.touchCoords.x,
-           y:clientY - game.touchCoords.y
+            x: clientX - game.touchCoords.x,
+            y: clientY - game.touchCoords.y
         };
         const threshold = 20;
         const diag_threshold = 14;
@@ -84,32 +95,39 @@ function swipeMove(e) {
             newX = setVelocityX(diff, clientX, clientY, newX, newY, 0);
             newY = setVelocityY(diff, clientX, clientY, newX, newY, 0);
 
-        }
-        else if (Math.abs(diff.x) > threshold) {
+        } else if (Math.abs(diff.x) > threshold) {
             newX = setVelocityX(diff, clientX, clientY, newX, newY, min);
             player.velocity.y = 0;
 
-        }
-        else if (Math.abs(diff.y) > threshold) {
+        } else if (Math.abs(diff.y) > threshold) {
             newY = setVelocityY(diff, clientX, clientY, newX, newY, min);
             player.velocity.x = 0;
         }
 
         if (newY != oldY || newX != oldX) {
-            let {x, y} = player.velocity;
+            let {
+                x,
+                y
+            } = player.velocity;
             console.log(`velocity ${x},${y}`);
             checkPlayer(oldX, oldY, newX, newY);
 
             if (!game.timer) {
-              let delay = 125;
-              game.timer = window.setTimeout(function() {
-                startCoast();
-              }, delay);
+                let delay = 125;
+                game.timer = window.setTimeout(function() {
+                    startCoast();
+                }, delay);
             }
+        } else {
+            let {
+                x,
+                y
+            } = player.velocity;
         }
-        else {
-           let {x, y} = player.velocity;
-        }
+
+    }
+
+
 }
 
 function stopCoast() {
@@ -120,51 +138,59 @@ function stopCoast() {
     game.timer = null;
     drawMap(0, 0, COLS, ROWS);
 }
+
 function startCoast() {
     let delay = 100;
     game.timer = window.setInterval(function() {
         inertia();
-    }, delay);         
+    }, delay);
 }
+
 function inertia() {
 
-    let {x, y} = player.velocity;
+    let {
+        x,
+        y
+    } = player.velocity;
 
     console.log(`inertia (${x},${y})`);
 
-    let {x:oldX, y:oldY} = player.coords;
+    let {
+        x: oldX,
+        y: oldY
+    } = player.coords;
 
-    let {x:newX, y:newY} = player.coords;
-   
+    let {
+        x: newX,
+        y: newY
+    } = player.coords;
+
     if (x && !player.edgeX(x)) {
-       newX = player.coords.x + x;
+        newX = player.coords.x + x;
     }
     if (y && !player.edgeY(y)) {
         newY = player.coords.y + y;
     }
     checkPlayer(oldX, oldY, newX, newY);
 }
+
 function checkPlayer(oldX, oldY, newX, newY) {
-        const freeTile = (x,y) => {
-            let tileCode = game.map[y][x];
-            let solidTiles = [WALL_CODE, ENEMY_CODE];
-            return !solidTiles.includes(tileCode);
-        }
-        if (freeTile(newX,newY)) {
-            movePlayer(newX, newY);
-        }
-        else if (newY != oldY && freeTile(oldX,newY)) {
-            movePlayer(oldX, newY); 
-        }
-        else if (newX != oldX && freeTile(newX,oldY)) {
-            movePlayer(newX, oldY); 
-        }
-        else if (game.map[newY][newX] == ENEMY_CODE) {
-            checkEnemy(newX, newY);   
-            stopCoast();
-        }
-        else {
-            console.log('nothing to do.');
-            stopCoast();
-        }
+    const freeTile = (x, y) => {
+        let tileCode = game.map[y][x];
+        let solidTiles = [WALL_CODE, ENEMY_CODE];
+        return !solidTiles.includes(tileCode);
+    }
+    if (freeTile(newX, newY)) {
+        movePlayer(newX, newY);
+    } else if (newY != oldY && freeTile(oldX, newY)) {
+        movePlayer(oldX, newY);
+    } else if (newX != oldX && freeTile(newX, oldY)) {
+        movePlayer(newX, oldY);
+    } else if (game.map[newY][newX] == ENEMY_CODE) {
+        checkEnemy(newX, newY);
+        stopCoast();
+    } else {
+        console.log('nothing to do.');
+        stopCoast();
+    }
 }
